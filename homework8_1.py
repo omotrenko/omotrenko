c611b7b6d67afafea87e69aedb7f023bca390346
omotrenko <v_n-h@mail.ru>
2023-06-04 15:52:42 +0300

    Дз3 задание 7

---
+++ homework8_1.py
@@ -0,0 +1,62 @@
+"""1. Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
+В рамках класса реализовать два метода. Первый, с декоратором @classmethod.
+Он должен извлекать число, месяц, год и преобразовывать их тип к типу «Число».
+Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12).
+Проверить работу полученной структуры на реальных данных."""
+
+
+class Data:
+    date_format = "dd-mm-yyyy"
+
+    def __init__(self, dmy_str):
+        self.data = dmy_str
+
+    def print_dmy_str(self):
+        print(self.data)
+
+    @classmethod
+    def print_data_format(cls):
+        print(f"data format is {cls.date_format}")
+
+    @classmethod
+    def from_str_to_int(cls, dmy_str):
+        dd, mm, yyyy = dmy_str.split(sep='-')
+        dd = int(dd)
+        mm = int(mm)
+        yyyy = int(yyyy)
+        return dd, mm, yyyy
+
+    @staticmethod
+    def valid_data(dd, mm, yyyy):
+        assert 0 < mm < 13, ValueError('Косяк!')
+        m30 = (4, 6, 9, 11)
+        if mm in m30:
+            assert 0 < dd < 31, ValueError('Косяк!')
+        elif mm != 2:
+            assert 0 < dd < 32, ValueError('Косяк!')
+        elif yyyy % 4 == 0:
+            assert 0 < dd < 30, ValueError('Косяк!')
+        else:
+            assert 0 < dd < 29, ValueError('Косяк!')
+
+
+dt1 = Data('5 января 91')
+dt1.print_dmy_str()
+
+Data.print_data_format()
+dt1.print_data_format()
+
+dd, mm, yyyy = Data.from_str_to_int("04-09-1887")
+try:
+    Data.valid_data(dd, mm, yyyy)
+except:
+    print("Wrong date values")
+
+
+dt_str = input()
+dd, mm, yyyy = Data.from_str_to_int(dt_str)
+try:
+    Data.valid_data(dd, mm, yyyy)
+except:
+    print(f"Wrong date values in {dt_str}")
+
